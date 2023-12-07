38068e9f428658d1bc02d2c6584f46ab0dc17a3a
TH-Corsetti <theocorsetti@free.fr>
2023-12-07 21:53:14 +0100

    Tous les tests complexes sont validés

--- complexe.c
+++ complexe.c
@@ -2,94 +2,97 @@
 #include <math.h>           // Pour certaines fonctions trigo notamment
 
 // Implantations de reelle et imaginaire
-/** FONCTION reelle À IMPLANTER **/
+
 double reelle (complexe_t z){
   return (double) z.reelle;
 }
 
-/** FONCTION imaginaire À IMPLANTER **/
+
 double imaginaire (complexe_t z){
   return (double) z.imaginaire;
   };
 
 // Implantations de set_reelle et set_imaginaire
-/** PROCÉDURE set_reelle À IMPLANTER **/
+
 void set_reelle (complexe_t *z, double a){
    (*z).reelle = a;
 }
 
-/** PROCÉDURE set_imaginaire À IMPLANTER **/
+
 void set_imaginaire (complexe_t *z, double a){
    (*z).imaginaire = a;
 }
 
-/** PROCÉDURE init À IMPLANTER **/
+
 void init (complexe_t *z, double a, double b){
     set_reelle (z, a);
     set_imaginaire (z, b);
 }
 
 // Implantation de copie
-/** PROCÉDURE copie À IMPLANTER */
+
 void copie (complexe_t *resultat, complexe_t autre) {
      (*resultat).reelle = autre.reelle;
      (*resultat).imaginaire = autre.imaginaire;
 }
 
 // Implantations des fonctions algébriques sur les complexes
-/** PROCÉDURE conjugue À IMPLANTER **/
+
 void conjugue (complexe_t *resultat, complexe_t op)
 {
   (*resultat).imaginaire = - op.imaginaire;
   (*resultat).reelle = op.reelle;
 }
 
-/** PROCÉDURE ajouter À IMPLANTER **/
+
 void ajouter (complexe_t *resultat, complexe_t gauche, complexe_t droite){
   (*resultat).reelle = gauche.reelle + droite.reelle;
   (*resultat).imaginaire = gauche.imaginaire + droite.imaginaire;
 }
 
-/** PROCÉDURE soustraire À IMPLANTER **/
+
 void soustraire (complexe_t *resultat, complexe_t gauche, complexe_t droite){
   (*resultat).reelle = gauche.reelle - droite.reelle;
   (*resultat).imaginaire = gauche.imaginaire - droite.imaginaire;
 }
 
-/** PROCÉDURE multiplier À IMPLANTER **/
+
 void multiplier (complexe_t *resultat, complexe_t gauche, complexe_t droite){
   (*resultat).reelle = gauche.reelle*droite.reelle - gauche.imaginaire*droite.imaginaire;
   (*resultat).imaginaire = gauche.reelle*droite.imaginaire + gauche.imaginaire*droite.reelle;
 }
 
-/** PROCÉDURE echelle À IMPLANTER **/
+
 void echelle (complexe_t *resultat, complexe_t op, double facteur) {
   (*resultat).reelle = op.reelle * facteur;
   (*resultat).imaginaire = op.imaginaire * facteur;
 }
 
-/** PROCÉDURE puissance À IMPLANTER **/
+
 void puissance (complexe_t *resultat, complexe_t op, int exposant){
-  double module_op = module(op);
-  double angle = argument (op) * exposant;
-  double module = pow (module_op, exposant);
-  (*resultat).reelle = module * cos(angle);
-  (*resultat).imaginaire = module * sin(angle);
+  if (exposant == 0) {
+    init(resultat,1,0);
+  } else {
+    copie(resultat,op);
+    for (int i=1;i<exposant;i++) {
+      multiplier(resultat, *resultat, op);
+    }  
+  } 
 }
 
 // Implantations du module et de l'argument
-/** FONCTION module_carre À IMPLANTER **/
+
 double module_carre (complexe_t z) {
   double module_carre = pow (module (z), 2);
   return module_carre;
 }
-/** FONCTION module À IMPLANTER **/
+
 double module (complexe_t z) {
   double module = sqrt (pow (z.reelle, 2) + pow (z.imaginaire, 2));
   return module;
 }
 
-/** FONCTION argument À IMPLANTER **/
+
 double argument (complexe_t z) {
   double theta;
   if (imaginaire(z) == 0){
