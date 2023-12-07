ba795086be94f56b75b61561ef8405e1346ab62e
TH-Corsetti <theocorsetti@free.fr>
2023-12-07 21:40:28 +0100

    Reste à vérifier le test de puissance

--- complexe.c
+++ complexe.c
@@ -3,28 +3,28 @@
 
 // Implantations de reelle et imaginaire
 /** FONCTION reelle À IMPLANTER **/
-float reelle (complexe_t z){
-  return z.reelle;
+double reelle (complexe_t z){
+  return (double) z.reelle;
 }
 
 /** FONCTION imaginaire À IMPLANTER **/
-float imaginaire (complexe_t z){
-  return z.imaginaire;
+double imaginaire (complexe_t z){
+  return (double) z.imaginaire;
   };
 
 // Implantations de set_reelle et set_imaginaire
 /** PROCÉDURE set_reelle À IMPLANTER **/
-void set_reelle (complexe_t *z, float a){
+void set_reelle (complexe_t *z, double a){
    (*z).reelle = a;
 }
 
 /** PROCÉDURE set_imaginaire À IMPLANTER **/
-void set_imaginaire (complexe_t *z, float a){
+void set_imaginaire (complexe_t *z, double a){
    (*z).imaginaire = a;
 }
 
 /** PROCÉDURE init À IMPLANTER **/
-void init (complexe_t *z, float a, float b){
+void init (complexe_t *z, double a, double b){
     set_reelle (z, a);
     set_imaginaire (z, b);
 }
@@ -59,7 +59,7 @@ void soustraire (complexe_t *resultat, complexe_t gauche, complexe_t droite){
 /** PROCÉDURE multiplier À IMPLANTER **/
 void multiplier (complexe_t *resultat, complexe_t gauche, complexe_t droite){
   (*resultat).reelle = gauche.reelle*droite.reelle - gauche.imaginaire*droite.imaginaire;
-  (*resultat).imaginaire = gauche.reelle*droite.imaginaire - gauche.imaginaire*droite.reelle;
+  (*resultat).imaginaire = gauche.reelle*droite.imaginaire + gauche.imaginaire*droite.reelle;
 }
 
 /** PROCÉDURE echelle À IMPLANTER **/
@@ -70,33 +70,48 @@ void echelle (complexe_t *resultat, complexe_t op, double facteur) {
 
 /** PROCÉDURE puissance À IMPLANTER **/
 void puissance (complexe_t *resultat, complexe_t op, int exposant){
-  float module_op = module(op);
-  float angle = argument (op) * exposant;
-  float module = pow (module_op, exposant);
-  (*resultat).reelle = module * cos (angle);
+  double module_op = module(op);
+  double angle = argument (op) * exposant;
+  double module = pow (module_op, exposant);
+  (*resultat).reelle = module * cos(angle);
   (*resultat).imaginaire = module * sin(angle);
 }
 
 // Implantations du module et de l'argument
 /** FONCTION module_carre À IMPLANTER **/
-float module_carre (complexe_t z) {
-  float module_carre = pow (module (z), 2);
+double module_carre (complexe_t z) {
+  double module_carre = pow (module (z), 2);
   return module_carre;
 }
 /** FONCTION module À IMPLANTER **/
-float module (complexe_t z) {
-  float module = sqrt (pow (z.reelle, 2) + pow (z.imaginaire, 2));
+double module (complexe_t z) {
+  double module = sqrt (pow (z.reelle, 2) + pow (z.imaginaire, 2));
   return module;
 }
 
 /** FONCTION argument À IMPLANTER **/
-float argument (complexe_t z) {
-  float theta;
-  if (z.reelle != 0) {
-      theta = atanf (z.imaginaire / z.reelle);
-    }
-  else {
+double argument (complexe_t z) {
+  double theta;
+  if (imaginaire(z) == 0){
+    if (reelle(z) >0){
       theta = 0;
-    }
+    } else{
+      theta = M_PI;
+    } 
+  } else if (reelle(z)>0) {
+      theta = atanf (imaginaire(z) / reelle(z));
+  } else if (reelle(z)<0) {
+      if (imaginaire(z) > 0) {
+        theta = atanf (imaginaire(z) / reelle(z)) + M_PI;
+      } else {
+        theta = atanf (z.imaginaire / z.reelle) - M_PI;
+      } 
+  } else if (z.reelle == 0) {
+        if (z.imaginaire > 0) {
+           theta = M_PI / 2;
+         } else {
+           theta = -M_PI / 2;
+      } 
+  }
   return theta;
 }
